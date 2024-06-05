f0c1ef79767d723d4cdce3a98cce76fbea9ac691
Enzo Zamberlan <enzo2017.bra@gmail.com>
2024-06-04 23:27:33 -0300

    Adicionei a mecânica de tiro da metralhadora

--- TezinGame/Assets/Scripts/WeaponScripts/Weapon.cs
+++ TezinGame/Assets/Scripts/WeaponScripts/Weapon.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Weapon : MonoBehaviour
-{   
+{
     public Transform firePoint;
     public GameObject bulletPrefab;
-    
+    public bool canSpamShoots = false;
+
     private GameManager _gameManager;
     private WeaponsStats stats;
     private SpriteRenderer spriteRender;
     private GameObject firepoint;
+    private Coroutine fireCoroutine;
+
+    [Header("Força do Tiro")]
+    public float fireForce = 20f;
+    [Header("Cadência de Tiro (balas por segundo)")]
+    public float fireRate = 10f;
+
 
     private void Awake()
     {
@@ -23,39 +30,73 @@ public class Weapon : MonoBehaviour
         _gameManager = FindObjectOfType<GameManager>();
     }
 
-    [Header("Força do Tiro")]
-    public float fireForce = 20f;
+    private void Update()
+    {   
+        if (canSpamShoots)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (fireCoroutine == null)
+                {
+                    fireCoroutine = StartCoroutine(FireContinuously());
+                }
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (fireCoroutine != null)
+                {
+                    StopCoroutine(fireCoroutine);
+                    fireCoroutine = null;
+                }
+            }
+        }
+
+    }
+
+    private IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            Fire();
+            yield return new WaitForSeconds(1f / fireRate);
+        }
+    }
 
     public void Fire()
     {
         Debug.Log("Fogo!!");
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
-   }
+    }
 
-   public void SwitchWeapon(string Weapon)
-   {
+    public void SwitchWeapon(string Weapon)
+    {
         if (stats.allStats.ContainsKey(Weapon))
-        {   
+        {
             transform.localScale = stats.allStats[Weapon]["Scale"];
 
-            if (firepoint.transform.position == new UnityEngine.Vector3(1.16f, 0.54f, 0f))
+            if (firepoint.transform.position == new Vector3(1.16f, 0.54f, 0f))
             {
-                firepoint.transform.position = new UnityEngine.Vector3(firepoint.transform.position.x + 0.27f, firepoint.transform.position.y, 0f);
+                firepoint.transform.position = new Vector3(firepoint.transform.position.x + 0.27f, firepoint.transform.position.y, 0f);
             }
 
             if (Weapon == "M4")
             {
                 spriteRender.sprite = _gameManager.M4Weapon;
-            } else if (Weapon == "Pistol"){
 
-                spriteRender.sprite = _gameManager.PistolWeapon;
+                canSpamShoots = true;
             }
-          
+            else if (Weapon == "Pistol")
+            {
+                spriteRender.sprite = _gameManager.PistolWeapon;
+
+                canSpamShoots = false;
+            }
         }
         else
         {
             Debug.LogError("Weapon not found: " + Weapon);
         }
-   }
+    }
 }
